c617056a5a6f2d92d415360850a59ca4b574dac5
Matt <matt2021@outlook.com>
2020-12-14 18:09:24 +0000

    Refactor bank authorization process

--- PaymentGateway/PaymentGateway/PaymentGateway.Domain/PaymentService.cs
+++ PaymentGateway/PaymentGateway/PaymentGateway.Domain/PaymentService.cs
@@ -12,28 +12,21 @@ namespace PaymentGateway.Domain
     public class PaymentService
     {
         private readonly IPaymentRepository _paymentRepository;
+        private readonly HttpClient _client;
+        string bankApiAddress = "http://app:8080/";
 
         public PaymentService(IPaymentRepository paymentRepository)
         {
             _paymentRepository = paymentRepository;
+            _client = new HttpClient();
         }
 
         public async Task<AuthorizationOutput> AuthorizeTransaction(AuthorizationInput authorizationInput)
         {
             //Do basic check of credit card data
             //Authorize transaction with bank
-            dynamic authorizationRequest = new
-            {
-                cardNumber = authorizationInput.Card.CardNumber,
-                amount = authorizationInput.Amount
-            };
-            var json = JsonConvert.SerializeObject(authorizationRequest);
-            var buffer = System.Text.Encoding.UTF8.GetBytes(json);
-            var byteContent = new ByteArrayContent(buffer);
-            HttpClient client = new HttpClient();
-            HttpResponseMessage response = await client.PostAsync("http://app:8080/authorize", byteContent);
-            dynamic responseContent = await response.Content.ReadAsAsync<object>();
-            if (!response.IsSuccessStatusCode || responseContent.authorizedAmount.ToString() == "0.000000")
+            decimal authorizedAmount = await GetAuthorizationFromBank(authorizationInput);
+            if (authorizedAmount == 0)
             {
                 return new AuthorizationOutput
                 {
@@ -44,7 +37,7 @@ namespace PaymentGateway.Domain
                     }
                 };
             }
-            Transaction transaction = await _paymentRepository.GetAuthorization(authorizationInput.Amount, 
+            Transaction transaction = await _paymentRepository.GetAuthorization(authorizationInput.Amount,
                 authorizationInput.Currency);
 
             AuthorizationOutput output = new AuthorizationOutput
@@ -52,13 +45,35 @@ namespace PaymentGateway.Domain
                 AuthorizationId = transaction.Id,
                 TransactionOutput = new TransactionOutput
                 {
-                    AmountAvailable = responseContent.authorizedAmount,
+                    AmountAvailable = authorizedAmount,
                     Currency = authorizationInput.Currency,
                     Error = null,
                     Success = true
                 }
             };
             return output;
+        }
+
+        private async Task<decimal> GetAuthorizationFromBank(AuthorizationInput authorizationInput)
+        {
+            ByteArrayContent byteContent = GetAuthorizationRequestByteContent(authorizationInput);
+            HttpResponseMessage response = await _client.PostAsync(bankApiAddress + "authorize", byteContent);
+            dynamic responseContent = await response.Content.ReadAsAsync<object>();
+            decimal authorizedAmount = !response.IsSuccessStatusCode ? 0 : responseContent.authorizedAmount;
+            return authorizedAmount;
+        }
+
+        private static ByteArrayContent GetAuthorizationRequestByteContent(AuthorizationInput authorizationInput)
+        {
+            dynamic authorizationRequest = new
+            {
+                cardNumber = authorizationInput.Card.CardNumber,
+                amount = authorizationInput.Amount
+            };
+            var json = JsonConvert.SerializeObject(authorizationRequest);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(json);
+            var byteContent = new ByteArrayContent(buffer);
+            return byteContent;
         }
 
         public async Task<TransactionOutput> CapturePayment(PaymentRequest paymentRequest)
